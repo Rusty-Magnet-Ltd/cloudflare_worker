5966339ad31a6bfcc3c7b823eef3fbcc3a33df42
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-02-21 16:46:49 +0000

    itty router added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
+import {
+	error,              // creates error Responses
+	json,               // creates JSON Responses
+	Router,             // the Router itself
+} from 'itty-router'
 
+const router = Router()
+
+router
+	// GET todos - just return some data!
+	.get('/', ( req ) => req.cf || error(404, 'That todo was not found'))
+
+	// return a 404 for anything else
+	.all('*', () => error(404))
+
+// Example showing Cloudflare module syntax
 export default {
-	async fetch(req) {
-		const cfobj =
-			req.cf !== undefined
-				? req.cf
-				: { error: "The `cf` object is not available inside the preview." };
-
-		// bot score has to be optional for free zones
-		const resp= {
-				asn_number: cfobj?.asn,
-				asn_name: cfobj?.asOrganization,
-				country: cfobj?.country,
-				bot_score: cfobj?.botManagement?.score,
-				attestation: req.headers.has('att'),
-				attestation_value: req.headers.get('att'),
-		}
-
-		let headers = new Headers();
-		headers.set('x-foo', '123');
-		headers.set("content-type", "application/json;charset=UTF-8")
-		return new Response(JSON.stringify(resp, null, 2), {
-			headers: headers,
-		});
-	},
-};
+	fetch: (req, env, ctx) => router
+		.handle(req, env, ctx)
+		.then(json)
+		.catch(error)
+}
