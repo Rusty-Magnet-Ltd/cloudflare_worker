19e075d5d4f0d2c9cc9871edb0afc6203c4b5bc6
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2024-04-02 16:19:37 +0100

    Upgrade dependencies (#6)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-import {
-	error,              // error Responses
-	json,               // JSON Responses
-	Router
-} from 'itty-router'
+import { AutoRouter } from 'itty-router' // ~1kB
 
-const router = Router()
+const router = AutoRouter()
 
 router
-	// GET todos - just return some data!
-	.get('/',
-		( req ) => req.cf || error(500, 'can\'t access Cloudflare object ')
-	)
+    .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+    .get('/json', () => [1,2,3])
+    .get('/promises', () => Promise.resolve('foo'))
 
-	// return a 404 for anything else
-	.all('*', () => error(404))
-
-// Example showing Cloudflare module syntax
-export default {
-	fetch: (req, env, ctx) => router
-		.handle(req, env, ctx)
-		.then(json)
-		.catch(error)
-}
+export default router
