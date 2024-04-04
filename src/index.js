44e77f6fc44faf8226c43aa4410ccb23f0e27fd1
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2024-04-04 10:36:10 +0100

    Added static site (#11)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
+import manifest from '__STATIC_CONTENT_MANIFEST'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('success'))
+/* static landing page */
+app.get('/static/*', serveStatic({ root: './', manifest }))
+app.get('/favicon.ico', serveStatic({ path: './favicon.ico' }))
+app.get('/', (c) => c.redirect('/static/home/index.html'))
+
+app.get('/health', (c) => c.text('success'))
 
 app.notFound((c) => {
     return c.text('RM custom 404 Message', 404)
