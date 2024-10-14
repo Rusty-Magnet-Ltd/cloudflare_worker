026f138107f357a87577b51faa9735538348f9f3
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-10-14 11:44:59 +0100

    oct2024v2

--- src/routes/carpark.tsx
+++ src/routes/carpark.tsx
@@ -1,8 +1,19 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const carpark = new Hono();
 
+export const customLogger = (message: string, headers: Record<string, string>) => {
+  for (const key in headers) {
+    const data = headers[key];
+    console.log(`${key}: ${data}`);
+  }
+};
+
+carpark.use(logger(customLogger));
+
 carpark.get("/carpark", (c) => {
+  customLogger("Headers", c.req.header());
   return c.html(
     <html>
       <head>
