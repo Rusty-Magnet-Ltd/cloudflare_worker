314a1f20133e594c4c222ca41857fceb5e507e25
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2024-10-21 11:13:10 +0100

    unit test example (#39)

--- src/model/payload.ts
+++ src/model/payload.ts
@@ -4,6 +4,7 @@ export function NewPayload(name: string): JWTPayload {
   return {
     sub: name,
     role: "admin",
+    iat: Math.floor(Date.now() / 1000), // Token issued now
     exp: Math.floor(Date.now() / 1000) + 60 * 5 // Token expires in 5 minutes
   };
 }
--- test/unit.test.ts
+++ test/unit.test.ts
@@ -1,7 +1,22 @@
-import { expect, it, describe } from "vitest";
+import { expect, it, assert, describe } from "vitest";
+import { NewPayload } from "../src/model/payload";
+import { JWTPayload } from "hono/utils/jwt/types";
 
-describe("unit test environment ready", () => {
-  it("vitest working", () => {
-    expect(1 === 1).toBeTruthy();
+describe("test new JWT Payloads ", () => {
+  it("Issue Date of JWT set to value lower than Expiry Date", () => {
+    const jwt: JWTPayload = NewPayload("foo");
+    if (jwt.iat && jwt.exp) {
+      expect.soft(jwt.iat).toBeLessThan(jwt.exp);
+    } else {
+      assert.fail("iat of exp were not defined");
+    }
+  });
+
+  it("The regular JWT Payloads will give undefined values", () => {
+    const jwt: JWTPayload = {
+      sub: "Bob"
+    };
+    expect(jwt.exp).not.toBeDefined();
+    expect(jwt.iat).not.toBeDefined();
   });
 });
