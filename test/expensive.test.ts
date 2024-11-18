43fb2555510399e2e63fef5843d497f6c3dcb00d
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-11-18 15:23:00 +0000

    skip test

--- test/expensive.test.ts
+++ test/expensive.test.ts
@@ -2,13 +2,13 @@ import { describe, expect, it } from "vitest";
 import app from "../src/routes";
 import { env } from "cloudflare:test";
 
-describe("test /expensive Rate Limit route", () => {
+describe.skip("test /expensive Rate Limit route", () => {
   it("one expensive request is aok", async () => {
     const res = await app.request("/expensive", {}, env);
     expect(res.status).toBe(200);
   });
 
-  it("five expensive requests nok. Rate limit fires ok", async () => {
+  it("skipped -> five expensive requests nok. Rate limit fires ok", async () => {
     const max: number = 5;
     const responseCodes = new Array(max) as Array<number>;
     let i = 0;
