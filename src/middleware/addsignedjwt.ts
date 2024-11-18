01a06263fc21e625622e8f202cae7c3863635146
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-11-18 13:48:30 +0000

    debugging verify failures in prod worker about expired tokens

--- src/middleware/addsignedjwt.ts
+++ src/middleware/addsignedjwt.ts
@@ -4,11 +4,11 @@ import type { JwtEnv } from "../types/api";
 import { Context, Next } from "hono";
 import { env } from "hono/adapter";
 
-const payload: JWTPayload = {
+export const employeePayload: JWTPayload = {
   sub: "Bob",
   role: "admin",
   department: "hr",
-  exp: Math.floor(Date.now() / 1000) + 60 * 5, // Token expires in 5 minutes
+  exp: Math.floor(Date.now() / 1000) + (60 * 5), // Token expires in 5 minutes
   iat: Math.floor(Date.now() / 1000),
   nbf: Math.floor(Date.now() / 1000)
 };
@@ -19,7 +19,7 @@ export async function SignPayload(p: JWTPayload, secret: string) {
 
 export async function addSignedJwtMiddleware(ctx: Context, next: Next) {
   const { SECRET_KEY, SECURITY_HEADER_NAME } = env<JwtEnv>(ctx);
-  const token = SignPayload(payload, SECRET_KEY);
+  const token = SignPayload(employeePayload, SECRET_KEY);
   ctx.res.headers.set(SECURITY_HEADER_NAME, await token);
   await next();
 }
