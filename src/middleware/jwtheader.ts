894e761d114649d7b88148560e08e1fcd3b9c454
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2024-11-18 11:01:33 +0000

    middleware header present| (#62)

---
+++ src/middleware/jwtheader.ts
@@ -0,0 +1,12 @@
+import { Context, Next } from "hono";
+import { env } from "hono/adapter";
+import type { JwtEnv } from "../types/api";
+
+export async function jwtHeaderPresentCheck(ctx: Context, next: Next) {
+  const { SECURITY_HEADER_NAME } = env<JwtEnv>(ctx);
+  const jwtHeader = ctx.req.header(SECURITY_HEADER_NAME.toLowerCase());
+  if (jwtHeader == undefined || jwtHeader === "") {
+    return ctx.text("jwt header required", 400);
+  }
+  await next();
+}
--- src/middleware/verifyjwt.ts
+++ src/middleware/verifyjwt.ts
@@ -1,9 +1,7 @@
-// import { validator } from "hono/dist/types/validator";
 import { verify } from "hono/jwt";
 import { Context, Next } from "hono";
 import { env } from "hono/adapter";
 import type { JwtEnv } from "../types/api";
-import { JwtTokenInvalid, JwtTokenSignatureMismatched, JwtTokenExpired } from "hono/utils/jwt/types";
 
 export async function VerifyPayload(payload: string | undefined, secret: string) {
   return await verify(
@@ -15,31 +13,14 @@ export async function VerifyPayload(payload: string | undefined, secret: string)
 
 export async function verifyJwtMiddleware(ctx: Context, next: Next) {
   const { SECRET_KEY, SECURITY_HEADER_NAME } = env<JwtEnv>(ctx);
+  // todo: remove the request get here
   const jwtToVerify = ctx.req.header(SECURITY_HEADER_NAME.toLowerCase());
   try {
+    console.log("JWT CHECK");
     const decodedPayload = await VerifyPayload(jwtToVerify, SECRET_KEY);
     console.debug(decodedPayload);
   } catch (error) {
     console.log(`[!]verify JWT failed ` + error);
-
-    switch (error) {
-      case error instanceof JwtTokenInvalid:
-        console.error("[!]invalid jwt.\n"
-          + "\tSECURITY_HEADER_NAME=" + SECURITY_HEADER_NAME + "\n"
-          + "\tJWT=" + jwtToVerify + "\n"
-          + error
-        );
-        break;
-      case error instanceof JwtTokenSignatureMismatched:
-        console.error("[!]jwt signature not valid");
-        break;
-      case error instanceof JwtTokenExpired:
-        console.error("[!]jwt expired");
-        break;
-      default:
-        console.error("[!]unhandled jwt error " + "\n" + error);
-        break;
-    }
     return ctx.text("jwt verify failed", 401);
   }
   await next();
