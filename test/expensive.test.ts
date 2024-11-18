b9d131e112a90558689ff02b29a7b9d563b6e9c6
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-11-18 14:52:34 +0000

    readme + ratelimit

--- test/expensive.test.ts
+++ test/expensive.test.ts
@@ -3,8 +3,20 @@ import app from "../src/routes";
 import { env } from "cloudflare:test";
 
 describe("test /expensive Rate Limit route", () => {
-  it("payload signing looks ok", async () => {
+  it("one expensive request is aok", async () => {
     const res = await app.request("/expensive", {}, env);
     expect(res.status).toBe(200);
+  });
+
+  it("five expensive requests nok. Rate limit fires ok", async () => {
+    const max: number = 5;
+    let i = 0;
+
+    for (i; i <= max; i++) {
+      const res = await app.request("/expensive", {}, env);
+      if (i == max) {
+        expect(res.status).toBe(429);
+      }
+    }
   });
 });
