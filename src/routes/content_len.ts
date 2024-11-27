b9f0dd340f20a018e5466a4d9b81d23a75785741
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-11-27 11:41:29 +0000

    pr2

--- src/routes/content_len.ts
+++ src/routes/content_len.ts
@@ -1,15 +1,16 @@
 import { Hono } from "hono";
 
 const contentLen = new Hono();
+const clKey = "Content-Length";
 
-function returnContentLenJson(conLength: string = "Content-Length not found") {
-  return JSON.stringify({ message: conLength });
+function returnContentLenJson(conLength: string = `${clKey} not found`) {
+  return JSON.stringify({ worker_calculated: `length of: ${conLength}` });
 }
 
-contentLen.get(
+contentLen.post(
   "/content",
   async (c) => {
-    const cl = c.req.header("Content-Length");
+    const cl = c.req.header(clKey);
     return c.json(returnContentLenJson(cl));
   });
 
